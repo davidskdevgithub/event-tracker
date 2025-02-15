6a6506d49ba0f9eacc888cc24b832a817ae928c8
davidsk <davidsk.dev@gmail.com>
2025-02-15 00:28:04 -0300

    scrollToCurrentTime

--- src/components/event-scenarios.tsx
+++ src/components/event-scenarios.tsx
@@ -2,12 +2,16 @@ import { SCENARIOS } from '../mocks/data';
 
 import { ROW_HEIGHT } from '../utils/config';
 
-export const EventScenarios: React.FC = () => {
+interface EventScenariosProps {
+  onTimeClick: () => void;
+}
+
+export const EventScenarios: React.FC<EventScenariosProps> = ({ onTimeClick }) => {
   return (
     <div className="flex-none w-30">
-      <div className={`${ROW_HEIGHT} flex items-center justify-center bg-gray-800 border border-gray-700`}>
+      <div onClick={onTimeClick} className={`${ROW_HEIGHT} flex items-center justify-center bg-gray-800 border border-gray-700`}>
         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
       </div>
       {SCENARIOS.map(scenario => (
--- src/components/events-container.tsx
+++ src/components/events-container.tsx
@@ -1,20 +1,34 @@
+import { useRef } from 'react';
+
 import { EventScenarios } from './event-scenarios';
 import { EventsGrid } from './events-grid';
 import { TimeSlotCell } from './time-slot-cell';
 import { EventSlotCell } from './event-slot-cell';
 import { CurrentTimeIndicator } from './current-time-indicator';
 
-import { TOTAL_COLUMNS } from '../utils/config';
+import { TOTAL_COLUMNS, START_TIME, TOTAL_MINUTES } from '../utils/config';
+import { timeToMinutes } from '../utils/time';
 import { TIME_SLOTS, SCENARIOS, EVENTS } from '../mocks/data';
 
 export const EventsContainer: React.FC = () => {
+
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+  const scrollToCurrentTime = () => {
+    const container = scrollContainerRef.current;
+    if (container) {
+      const currentTimePosition = container.scrollWidth * (timeToMinutes("17:27") - START_TIME) / TOTAL_MINUTES;
+      container.scrollTo({ left: currentTimePosition - (container.clientWidth / 2), behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className="flex">
       {/* Columna fija de escenarios */}
-      <EventScenarios />
+      <EventScenarios onTimeClick={scrollToCurrentTime} />
 
       {/* Área desplazable */}
-      <div className="flex-grow overflow-x-auto">
+      <div ref={scrollContainerRef} className="flex-grow overflow-x-auto">
         <div className="inline-block min-w-full relative">
           {/* Time Indicator */}
           <CurrentTimeIndicator />
